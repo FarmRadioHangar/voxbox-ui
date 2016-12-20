67709522cc342364ee46e99986f315bad9d9536d
Johannes Hilden <johannes@isomorphic.co>
2016-12-20 15:02:34 +0300

    New branch

---
+++ src/store.js
@@ -0,0 +1,47 @@
+var initialMixerState = {};
+
+function mixer(state, action) {
+  if ('undefined' === typeof state) {
+    return initialMixerState;
+  }
+  switch (action.type) {
+    case 'APP_INITIALIZE':
+      var channels = action.data.channels;
+      return {
+        channelList: Object.keys(channels).map(function(key) {
+          var channel = channels[key];
+          channel.id = key;
+          return channel;
+        })
+      };
+    default:
+      return state;
+  }
+}
+
+var initialMessagesState = {};
+
+function messages(state, action) {
+  if ('undefined' === typeof state) {
+    return initialMessagesState;
+  }
+  switch (action.type) {
+    case 'INBOX_MESSAGES':
+      var ids = action.data.ids;
+      var messages = action.data.messages;
+      return {
+        inbox: ids.map(function(id) {
+          return messages[id];
+        }).filter(function(msg) {
+          return 'sms_in' === msg.type;
+        })
+      };
+    default:
+      return state;
+  }
+}
+
+var store = Redux.createStore(Redux.combineReducers({
+  mixer: mixer,
+  messages: messages,
+}));
